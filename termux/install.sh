2d24310efd44fcd419e0a4607ed22e79e8a00cff
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2021-08-29 18:43:31 +0800

    Update install.sh

--- termux/install.sh
+++ termux/install.sh
@@ -1,1 +1,1 @@
-pkg i git nodejs yarn -y && npm install -g pm2 && yarn global add pm2 && git clone https://github.com/elecV2/elecV2P.git && cd elecV2P && yarn && cd ~ && wget -N "https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/proxypool.sh" && chmod +x proxypool.sh && rm install.sh
+pkg i git nodejs yarn -y && npm install -g pm2 && yarn global add pm2 && git clone https://github.com/elecV2/elecV2P.git && cd elecV2P && yarn && cd ~ && wget -N "https://raw.githubusercontent.com/shoujiyanxishe/elecV2Psh/main/termux/v2p.sh" && chmod +x v2p.sh && rm install.sh
