c2eb888013323352cf2c31a9c7af465404dbb102
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2022-01-05 13:51:21 +0800

    Update install.sh

--- termux/install.sh
+++ termux/install.sh
@@ -1,8 +1,7 @@
-sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
-sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
-sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
-pkg up -y
-pkg i git nodejs yarn -y
+apt update
+apt upgrade -y
+apt update
+apt install git nodejs yarn -y
 npm install -g pm2
 yarn global add pm2
 git clone https://github.com/elecV2/elecV2P.git
